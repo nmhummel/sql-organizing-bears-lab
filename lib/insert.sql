72e6665a220090cbd5b2561ecb6e669dc998e7ba
nmhummel <natalie.hummel@gmail.com>
2020-11-16 15:33:14 -0600

    commit tree cc60a3b223e6a637033c17e7f03c330e49c2a010

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "chocolate brown", "lover", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "tawny", "fighter", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "adobe", "sassy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "dark brown", "aloof", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "F", "tawny", "aloof", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "F", "chocolate brown", "fighter", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 8, "F", "adobe", "lover", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, "M", "dark brown", "fighter", 1);
